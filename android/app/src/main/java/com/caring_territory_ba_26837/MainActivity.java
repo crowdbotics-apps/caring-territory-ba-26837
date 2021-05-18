b1ae95ec63d7edc50d3b99ff7bb95bb4395dc43c
Crowdbotics <team@crowdbotics.com>
2021-05-18 01:42:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/caring_territory_ba_26837/MainActivity.java
@@ -0,0 +1,15 @@
+package com.caring_territory_ba_26837;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "caring_territory_ba_26837";
+  }
+}
